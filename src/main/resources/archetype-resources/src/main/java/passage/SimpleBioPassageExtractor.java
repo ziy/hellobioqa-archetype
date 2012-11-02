447260b09b7cdf0d1d70eec4be725479d279be0b
ziy <sincerezi@gmail.com>
2012-11-01 20:02:41 -0400

    Updated hellobioqa

---
+++ src/main/resources/archetype-resources/src/main/java/passage/SimpleBioPassageExtractor.java
@@ -0,0 +1,58 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.passage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import org.jsoup.Jsoup;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+
+import edu.cmu.lti.oaqa.framework.data.Keyterm;
+import edu.cmu.lti.oaqa.framework.data.PassageCandidate;
+import edu.cmu.lti.oaqa.framework.data.RetrievalResult;
+import ${groupId}.hello.passage.KeytermWindowScorerSum;
+import ${groupId}.hello.passage.PassageCandidateFinder;
+import ${groupId}.hello.passage.SimplePassageExtractor;
+
+public class SimpleBioPassageExtractor extends SimplePassageExtractor {
+
+  @Override
+  protected List<PassageCandidate> extractPassages(String question, List<Keyterm> keyterms,
+          List<RetrievalResult> documents) {
+    List<PassageCandidate> result = new ArrayList<PassageCandidate>();
+    for (RetrievalResult document : documents) {
+      System.out.println("RetrievalResult: " + document.toString());
+      String id = document.getDocID();
+      try {
+        String htmlText = wrapper.getDocText(id);
+
+        // cleaning HTML text
+        String text = Jsoup.parse(htmlText).text().replaceAll("([${symbol_escape}177-${symbol_escape}377${symbol_escape}0-${symbol_escape}32]*)", "")/* .trim() */;
+        // for now, making sure the text isn't too long
+        text = text.substring(0, Math.min(5000, text.length()));
+        System.out.println(text);
+
+        PassageCandidateFinder finder = new PassageCandidateFinder(id, text,
+                new KeytermWindowScorerSum());
+        List<String> keytermStrings = Lists.transform(keyterms, new Function<Keyterm, String>() {
+          public String apply(Keyterm keyterm) {
+            return keyterm.getText();
+          }
+        });
+        List<PassageCandidate> passageSpans = finder.extractPassages(keytermStrings
+                .toArray(new String[0]));
+        for (PassageCandidate passageSpan : passageSpans)
+          result.add(passageSpan);
+      } catch (SolrServerException e) {
+        e.printStackTrace();
+      }
+    }
+    return result;
+  }
+
+}
